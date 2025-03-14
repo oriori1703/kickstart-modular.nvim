b38a6a068d85f802bc9827e3d108b382b265c5cd
orip <oriori1703@gmail.com>
2025-03-14 23:41:45 +0200

    Add lsp for bash

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -283,6 +283,8 @@ return {
             },
           },
         },
+
+        bashls = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -302,6 +304,8 @@ return {
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'markdownlint',
+        'shellcheck',
+        'shfmt',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 

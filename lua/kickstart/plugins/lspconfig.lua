c5f6090b17ebd69e631a32e24e487bf8c504888f
orip <oriori1703@gmail.com>
2025-03-15 18:01:23 +0200

    Add lsp for php

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -268,6 +268,7 @@ return {
             },
           },
         },
+
         yamlls = {
           settings = {
             yaml = {
@@ -285,6 +286,8 @@ return {
         },
 
         bashls = {},
+
+        phpactor = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -304,8 +307,12 @@ return {
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'markdownlint',
+        -- bash
         'shellcheck',
         'shfmt',
+        -- php
+        'phpcs',
+        'php-cs-fixer',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 

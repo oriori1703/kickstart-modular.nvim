e065fb4f97e67cf24f0babed9cc93d00757f8ed4
orip <oriori1703@gmail.com>
2025-04-18 18:24:55 +0300

    Allow single line function in stylua

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -29,59 +29,43 @@ return {
     -- Basic debugging keymaps, feel free to change to your liking!
     {
       '<F5>',
-      function()
-        require('dap').continue()
-      end,
+      function() require('dap').continue() end,
       desc = 'Debug: Start/Continue',
     },
     {
       '<F1>',
-      function()
-        require('dap').step_into()
-      end,
+      function() require('dap').step_into() end,
       desc = 'Debug: Step Into',
     },
     {
       '<F2>',
-      function()
-        require('dap').step_over()
-      end,
+      function() require('dap').step_over() end,
       desc = 'Debug: Step Over',
     },
     {
       '<F3>',
-      function()
-        require('dap').step_out()
-      end,
+      function() require('dap').step_out() end,
       desc = 'Debug: Step Out',
     },
     {
       '<leader>b',
-      function()
-        require('dap').toggle_breakpoint()
-      end,
+      function() require('dap').toggle_breakpoint() end,
       desc = 'Debug: Toggle Breakpoint',
     },
     {
       '<leader>B',
-      function()
-        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-      end,
+      function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end,
       desc = 'Debug: Set Breakpoint',
     },
     {
       '<Leader>lp',
-      function()
-        require('dap').set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
-      end,
+      function() require('dap').set_breakpoint(nil, nil, vim.fn.input 'Log point message: ') end,
       desc = 'Debug: Set Logpoint',
     },
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     {
       '<F7>',
-      function()
-        require('dapui').toggle()
-      end,
+      function() require('dapui').toggle() end,
       desc = 'Debug: See last session result.',
     },
   },

95e530c163da6144fef412fdcaeafcce085d4e8f
orip <oriori1703@gmail.com>
2025-04-18 18:24:55 +0300

    Allow single line function in stylua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -37,9 +37,7 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
-vim.keymap.set('n', 'zt', function()
-  vim.wo.spell = not vim.o.spell
-end, { desc = 'Toggle spell checking' })
+vim.keymap.set('n', 'zt', function() vim.wo.spell = not vim.o.spell end, { desc = 'Toggle spell checking' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -50,9 +48,7 @@ end, { desc = 'Toggle spell checking' })
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
+  callback = function() vim.hl.on_yank() end,
 })
 
 -- vim: ts=2 sts=2 sw=2 et
